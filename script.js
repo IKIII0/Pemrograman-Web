76b5d448caf79f2089902d230cfb6fe22c64d6c0
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-09-06 15:42:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let prevScroll = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScroll = window.pageYOffset;
+  const navbar = document.getElementById("navbar");
+
+  console.log("prevScroll: ", prevScroll);
+  console.log("currentScroll: ", currentScroll);
+
+  if (prevScroll > currentScroll) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-80px";
+  }
+  prevScroll = currentScroll;
+};
